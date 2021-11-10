29abbcf8706357330dfa5ff4d7b9b440ba03f12e
Tohir Raihan <tohir.raihan@gmail.com>
2021-11-10 18:43:41 +0600

    remove some photos

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Home from './Pages/Home/Home/Home';
 import AuthProvider from './context/AuthProvider';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Login from './Pages/Login/Login';
 
 function App() {
     return (
@@ -13,6 +14,14 @@ function App() {
                     <Switch>
                         <Route exact path="/">
                             <Home />
+                        </Route>
+
+                        <Route exact path="/home">
+                            <Home />
+                        </Route>
+
+                        <Route exact path="/login">
+                            <Login />
                         </Route>
                     </Switch>
                 </BrowserRouter>

761d2ae797f869848ffb6f45a7a747dd9fc5666e
Tohir Raihan <tohir.raihan@gmail.com>
2021-11-13 03:08:44 +0600

    update Home page

---
+++ src/Pages/Home/Features/Features.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { Col, Container, Row } from 'react-bootstrap';
+import { faBiking, faCarAlt, faTaxi, faTools } from '@fortawesome/free-solid-svg-icons';
+
+const Features = () => {
+    return (
+        <Container className='text-white text-center'>
+            <Row>
+                <Col md={4} className='bg-danger p-5'>
+                    <FontAwesomeIcon className='h1 mb-4' icon={faCarAlt} />
+                    <h3 className='fw-bold h4 mb-3'>Buy and Sale Vehiclese</h3>
+                    <p className='fw-light'>
+                        We buy and sale barand new car and also used car of any
+                        brand
+                    </p>
+                </Col>
+                <Col md={4} className='bg-primary p-5'>
+                    <FontAwesomeIcon className='h1 mb-4' icon={faBiking} />
+                    <h3 className='fw-bold h4 mb-3'>Test Driving</h3>
+                    <p className='fw-light'>
+                        You can order a test drive for 5 hours before actually buy the car
+                    </p>
+                </Col>
+                <Col md={4} className='bg-dark p-5'>
+                    <FontAwesomeIcon className='h1 mb-4' icon={faTools} />
+                    <h3 className='fw-bold h4 mb-3'>Vehicles Servicing</h3>
+                    <p className='fw-light'>
+                        We give after sale servicing and in total we give five servicing
+                    </p>
+                </Col>
+
+            </Row>
+        </Container>
+    );
+};
+
+export default Features;
--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -3,12 +3,18 @@ import Cars from '../../Shared/Cars/Cars/Cars';
 import Footer from '../../Shared/Footer/Footer';
 import Navigation from '../../Shared/Navigation/Navigation';
 import Banner from '../Banner/Banner';
+import Features from '../Features/Features';
+import './Home.css';
 
 const Home = () => {
     return (
         <>
             <Navigation />
             <Banner />
+            <section className="features pb-5">
+                <Features />
+            </section>
+
             <section className="buy-cars my-5">
                 <h1 className="text-center mb-4">Buy Your Dream Car</h1>
                 <Cars isHome={true} />
